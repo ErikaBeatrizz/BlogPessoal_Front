7f9e4e5faa31ecbfd93dbbcaf96e81efa457d4d8
ErikaBeatrizz <erikabeatriz_bea@hotmail.com>
2022-11-22 17:06:52 -0300

    feat: create project

---
+++ src/componentes/Text/Text.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Text.css'
+
+function Text() {
+  return (
+    <p>
+    Teste de texto! 
+    </p>
+  )
+}
+
+export { Text }
---
+++ src/componentes/Title/Title.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Title.css';
+
+function Title (props: any) {
+    return (
+        <h1> Hello World! </h1>
+    )
+
+}
+
+export { Title }
