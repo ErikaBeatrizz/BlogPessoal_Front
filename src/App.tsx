1a91e46443e9df9861cb09eba7cefc7be263a450
ErikaBeatrizz <erikabeatriz_bea@hotmail.com>
2022-11-25 12:23:48 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
-import Navbar from './componentes/navbar/Navbar';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import { About } from './paginas/about/About';
+import { Home } from './paginas/home/Home';
+
 
 function App() {
   const nome = 'Erika';
   return (
-    <>    
-    <Navbar />    
-    </>
+      
+    <Router> // orquestrador - monta a pagina que o user vai ver
+    <Navbar />
+    <Routes> // lista de rotas da aplicação
+      <Route path='/' element={<Home />}> </Route>
+      <Route path='/home' element={<Home />}> </Route>
+      <Route path='/about' element={<About />}> </Route>
+
+    </Routes>     
+    </Router>  
+    
   );
 }
 
--- src/componentes/estaticos/navbar/Navbar.tsx
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -8,6 +8,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -34,7 +35,10 @@ export default function Navbar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+          <Link to='/home'> Home </Link>
+          </Typography>
+          <Typography variant="h6" className={classes.title}>
+          <Link to='/about'> About </Link>
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
