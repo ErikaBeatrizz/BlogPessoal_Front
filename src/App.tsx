469ff73e9a756d9ce4f7b78d05069ac822da6e36
ErikaBeatrizz <erikabeatriz_bea@hotmail.com>
2022-11-29 16:41:39 -0300

    feat: added state hook

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ function App() {
       <Route path='/' element={<Home />}> </Route>
       <Route path='/home' element={<Home />}> </Route>
       <Route path='/about' element={<About />}> </Route>
-
     </Routes>     
     </Router>  
     
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
 import './Home.css';
 
 function Home () {
+    const [num, setNum]  = useState(0); // HOOK - criar hook de estado
+
     return (
-        <h1> Página Home </h1>
+    <>
+        <h1> Você clicou {num} vezes </h1>
+        <Button variant="contained" onClick={() => setNum(num - 1)}>
+           -
+        </Button>
+        <Button variant="contained" onClick={() => setNum(num + 1)}>
+           +
+        </Button>
+        
+    </>
     );
 }
 
