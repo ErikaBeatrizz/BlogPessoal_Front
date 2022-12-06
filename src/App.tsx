902c5a4935864733f11fcb9903d584a026dd5cad
ErikaBeatrizz <erikabeatriz_bea@hotmail.com>
2022-12-06 16:34:46 -0300

    feat: redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import { About } from './paginas/about/About';
 import { Home } from './paginas/home/Home';
+import { store } from './store/store';
 
 
 function App() {
   const nome = 'Erika';
   return (
-      
-    <Router> // orquestrador - monta a pagina que o user vai ver
-    <Navbar />
-    <Routes> // lista de rotas da aplicação
-      <Route path='/' element={<Home />}> </Route>
-      <Route path='/home' element={<Home />}> </Route>
-      <Route path='/about' element={<About />}> </Route>
-    </Routes>     
-    </Router>  
+    // (router) orquestrador - monta a pagina que o user vai ver 
+    <Provider store={store}>      
+      <Router> 
+        <Navbar />
+          <Routes> // lista de rotas da aplicação
+            <Route path='/' element={<Home />}> </Route>
+            <Route path='/home' element={<Home />}> </Route>
+            <Route path='/about' element={<About />}> </Route>
+          </Routes>     
+      </Router> 
+    </Provider> 
     
   );
 }
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,9 +1,18 @@
 import { Button } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { changeClicks } from "../../store/clicks/actions";
+import { clicksState } from "../../store/clicks/clicksReducer";
 import './Home.css';
 
 function Home () {
-    const [num, setNum]  = useState(0); // HOOK - criar hook de estado
+     // const [num, setNum]  = useState(0); // HOOK - criar hook de estado
+    const num = useSelector<clicksState, clicksState['clicks']>(
+        (state) =>  state.clicks
+    );
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
         document.title = `Clicou ${num} vezes`;
@@ -16,10 +25,10 @@ function Home () {
     return (
     <>
         <h1> Você clicou {num} vezes </h1>
-        <Button variant="contained" onClick={() => setNum(num - 1)}>
+        <Button variant="contained" onClick={() => dispatch(changeClicks(num - 1))}>
            -
         </Button>
-        <Button variant="contained" onClick={() => setNum(num + 1)}>
+        <Button variant="contained" onClick={() => dispatch(changeClicks(num + 1))}>
            +
         </Button>
         
